f11caff81df7892a1f3e15a12dc27336ddd71105
DiegoJRR <diegojrr00@gmail.com>
2022-03-04 18:00:35 -0600

    actividad en clase de caminatas aleatorias

--- Ejercicios clase/caminata_aleatoria.py
+++ Ejercicios clase/caminata_aleatoria.py
@@ -1,8 +1,7 @@
 import numpy as np
 import random
+from graphs import graph_3d, graph_2d
 import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-
 
 #Caminatas aleatorias
 
@@ -11,25 +10,31 @@ import plotly.graph_objects as go
 #Diego de Jesus Ramirez Rodriguez A00828821
 #René Francisco Basáñez Córdoba A01700413
 
-#Cantidad de pasos a dar
-steps = 500
-#dimensiones
-dim = 3
-#Longitud máxima de los pasos de -L a L
-L = 5
+def caminata_1(steps, dim, L):
+    pos = np.zeros((steps, dim))
+    for i in range(1,steps):
+        for j in range(0, dim):
+            l = random.randint(-1,1)*L
+            pos[i,j] = pos[i-1,j] + l
 
-pos = np.zeros((steps, dim))
-for i in range(1,steps):
-    for j in range(0, dim):
-        l = random.randint(-L,L)
-        pos[i,j] = pos[i-1,j] + l
+    return pos
 
+def caminata_2(steps, dim, L):
+    pos = np.zeros((steps, dim))
+    for i in range(1,steps):
+        for j in range(0, dim):
+            l = -L + 2*L*np.random.uniform(0, 1)
+            pos[i,j] = pos[i-1,j] + l
 
-# Grafica 3D de la caminata
-fig = go.Figure(data=[go.Scatter3d(x=pos[:, 0], y=pos[:, 1], z=pos[:, 2],
-                                   mode='markers', marker=dict(
-        size=6,
-        colorscale='Viridis',
-        opacity=0.8
-    ))])
-fig.show()
+    return pos
+   
+fig, axs = plt.subplots(2, 3)
+N = 10000
+for i in range(0, 3):
+    pos_1 = caminata_1(N, 2, 5)
+    pos_2 = caminata_2(N, 2, 5)
+    
+    graph_2d(pos_1[:, 0], pos_1[:, 1], f"Caminata 1, iteración{i + 1}", axs[0, i])
+    graph_2d(pos_2[:, 0], pos_2[:, 1], f"Caminata 2, iteración {i + 1}", axs[1, i])
+
+plt.show()
